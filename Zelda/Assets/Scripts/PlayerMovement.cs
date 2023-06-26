79cc1ac043a2aaca8114e3eb88f0b4805554c07b
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-06-26 14:13:58 +0300

    Enemy Animations Setup
    Small Vector fix, Small Text fix, Enemy Animations,Animator,Blend Tree

--- Zelda/Assets/Scripts/PlayerMovement.cs
+++ Zelda/Assets/Scripts/PlayerMovement.cs
@@ -74,6 +74,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
         transform.position + (change * speed * Time.deltaTime) );
     }
