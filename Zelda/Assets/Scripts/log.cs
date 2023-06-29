d1ef37052dee179481d1ebe9282b23f521d0bd31
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-06-30 02:18:19 +0300

    Enemy States
    Small fix,enum,inheritance

--- Zelda/Assets/Scripts/log.cs
+++ Zelda/Assets/Scripts/log.cs
@@ -13,12 +13,12 @@ public class log : Enemy
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyState.idle;
         myRigidbody= GetComponent<Rigidbody2D>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
     void FixedUpdate()
     {
         CheckDistance();
@@ -30,8 +30,21 @@ public class log : Enemy
             transform.position) <= chaseRadius
             && Vector3.Distance(target.position,transform.position)>attackRadius)
         {
-            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-            myRigidbody.MovePosition(temp);
+            if(currentState == EnemyState.idle || currentState == EnemyState.walk
+                &&currentState!=EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+            }         
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if(currentState != newState)
+        {
+            currentState = newState;
         }
     }
 }
