87f401347317559035f278f30a7c262cf01d1364
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-07-02 18:33:38 +0300

    Heart System
    UI,Scriptable Objects,Event System

--- Zelda/Assets/Scripts/PlayerMovement.cs
+++ Zelda/Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
-
+    public FloatValue currentHealth;
+    public SignalSender playerHealthSignal;
 
     // Start is called before the first frame update
     void Start()
@@ -82,9 +83,14 @@ public class PlayerMovement : MonoBehaviour
         transform.position + (change * speed * Time.deltaTime) );
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime,float damage)
     {
-        StartCoroutine(KnockCo(knockTime));
+        currentHealth.initialValue -= damage;
+        if(currentHealth.initialValue > 0)
+        {
+            playerHealthSignal.Raise();
+            StartCoroutine(KnockCo(knockTime));
+        }
     }
 
     private IEnumerator KnockCo(float knockTime)
