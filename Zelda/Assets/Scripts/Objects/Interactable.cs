b8c255c2442a5d0bddc36627c6881273a74ed3e2
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-07-18 20:33:04 +0300

    Interactable Main Class
    Inherit

--- Zelda/Assets/Scripts/Objects/Interactable.cs
+++ Zelda/Assets/Scripts/Objects/Interactable.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
-public class Sign : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     public SignalSender context;
-    public GameObject dialogBox;
-    public TextMeshProUGUI dialogText;
-    public string dialog;
     public bool playerInRange;
 
     // Start is called before the first frame update
@@ -21,21 +16,8 @@ public class Sign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
-        {
-            if (dialogBox.activeInHierarchy)
-            {
-                dialogBox.SetActive(false);
-            }
-            else
-            {
-                dialogBox.SetActive(true);
-                dialogText.text = dialog;
-            }
 
-        }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
@@ -50,8 +32,7 @@ public class Sign : MonoBehaviour
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             context.Raise();
-            playerInRange= false;
-            dialogBox.SetActive(false);
+            playerInRange = false;
         }
     }
 }
