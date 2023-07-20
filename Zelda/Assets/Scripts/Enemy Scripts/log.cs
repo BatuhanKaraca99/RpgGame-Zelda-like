5ba257e4555f7e355a34b2070488f4ba2ddf7c64
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-07-20 14:15:05 +0300

    Patrolling Enemies - log
    Patrol Log script inherits log script,Array System for paths

--- Zelda/Assets/Scripts/Enemy Scripts/log.cs
+++ Zelda/Assets/Scripts/Enemy Scripts/log.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class log : Enemy
 {
-    private Rigidbody2D myRigidbody;
+    public Rigidbody2D myRigidbody; //to call
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
@@ -18,6 +18,7 @@ public class log : Enemy
         myRigidbody= GetComponent<Rigidbody2D>();
         anim= GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
+        anim.SetBool("wakeUp", true);
     }
 
     // Update is called once per frame
@@ -26,7 +27,7 @@ public class log : Enemy
         CheckDistance();
     }
 
-    void CheckDistance()
+    public virtual void CheckDistance()
     {
         if(Vector3.Distance(target.position,
             transform.position) <= chaseRadius
@@ -54,7 +55,7 @@ public class log : Enemy
         anim.SetFloat("moveY", setVector.y);
     }
 
-    private void changeAnim(Vector2 direction)
+    public void changeAnim(Vector2 direction) //to call
     {
         if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
         {
